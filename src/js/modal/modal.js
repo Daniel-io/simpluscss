7ddc5476a55d0d1ae2d6040fa8255f0030e3b6bf
Daniel <daniel.arenas.arango@gmail.com>
2020-06-25 22:06:10 -0500

    update modal

--- src/js/modal/modal.js
+++ src/js/modal/modal.js
@@ -12,11 +12,18 @@
 
 
 /**
- * 1. Modal button includes: data-toggle="modal", data-target="#yourID"
- * 2. Modal includes: ID, class: js-modal
- * 3. c-modal__content includes: js-modalContent
- * 4. Close button includes: data-dismiss="modal"
- * 5. data-target should include "#"" ex:  data-target="#example"
+ * 1. Modal button includes                : data-toggle="modal", data-target="#yourID"
+ * 2. Modal includes                       : the modal ID and class: js-modal
+ * 3. c-modal__content includes            : js-modalContent  
+ * 4. Close button includes                : data-dismiss="modal"
+ * 5. data-target should include "#"" ex   : data-target="#example"
+ */
+
+
+/**
+ * ##OPTIONS
+ * 1. data-backdrop="static" - Prevents modal close by clicking outside
+ * 
  */
 
 
@@ -132,6 +139,13 @@ const simplusModal = () => {
   //Dismiss modal by outside click
   toggle(selector.MODAL, 'click', function (event) {
 
+    //If static option enabled do not close modal by clicking outside
+    if (this.hasAttribute('data-backdrop')) {
+      if (this.dataset.backdrop === 'static') {
+        return;
+      } 
+    }
+    
     //If it can't find class name in ancestry the click is outside
     const isOutside = !event.target.closest('.js-modalContent');
     
